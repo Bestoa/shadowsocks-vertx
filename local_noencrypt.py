a941c502797164b1cd32f6538c5ca2f2c2c6b7be
Besto Apache <bestoapache@gmail.com>
2016-03-21 10:47:23 +0800

    Test no encrypt function

---
+++ local_noencrypt.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2012 clowwindy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys
+import json
+import socket
+import select
+import SocketServer
+import struct
+import os
+import logging
+import getopt
+
+def send_all(sock, data):
+    bytes_sent = 0
+    while True:
+        r = sock.send(data[bytes_sent:])
+        if r < 0:
+            return r
+        bytes_sent += r
+        if bytes_sent == len(data):
+            return bytes_sent
+
+
+class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    allow_reuse_address = True
+
+
+class Socks5Server(SocketServer.StreamRequestHandler):
+    def handle_tcp(self, sock, remote):
+        try:
+            fdset = [sock, remote]
+            while True:
+                r, w, e = select.select(fdset, [], [])
+                if sock in r:
+                    data = sock.recv(4096)
+                    if len(data) <= 0:
+                        break
+                    result = send_all(remote, data)
+                    if result < len(data):
+                        raise Exception('failed to send all data')
+
+                if remote in r:
+                    data = remote.recv(4096)
+                    if len(data) <= 0:
+                        break
+                    result = send_all(sock, data)
+                    if result < len(data):
+                        raise Exception('failed to send all data')
+        finally:
+            sock.close()
+            remote.close()
+
+    def send(self, sock, data):
+        sock.send(data)
+
+    def handle(self):
+        try:
+            sock = self.connection
+            sock.recv(262)
+            sock.send("\x05\x00")
+            data = self.rfile.read(4) or '\x00' * 4
+            mode = ord(data[1])
+            if mode != 1:
+                logging.warn('mode != 1')
+                return
+            addrtype = ord(data[3])
+            addr_to_send = data[3]
+            if addrtype == 1:
+                addr_ip = self.rfile.read(4)
+                addr = socket.inet_ntoa(addr_ip)
+                addr_to_send += addr_ip
+            elif addrtype == 3:
+                addr_len = self.rfile.read(1)
+                addr = self.rfile.read(ord(addr_len))
+                addr_to_send += addr_len + addr
+            elif addrtype == 4:
+                addr_ip = self.rfile.read(16)
+                addr = socket.inet_ntop(socket.AF_INET6, addr_ip)
+                addr_to_send += addr_ip
+            else:
+                logging.warn('addr_type not support')
+                # not support
+                return
+            addr_port = self.rfile.read(2)
+            addr_to_send += addr_port
+            port = struct.unpack('>H', addr_port)
+            try:
+                reply = "\x05\x00\x00\x01"
+                reply += socket.inet_aton('0.0.0.0') + struct.pack(">H", 2222)
+                self.wfile.write(reply)
+                # reply immediately
+                if '-6' in sys.argv[1:]:
+                    remote = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+                    remote.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                    remote.connect((SERVER, REMOTE_PORT, 0, 0))
+                else:
+                    remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    remote.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                    remote.connect((SERVER, REMOTE_PORT))
+
+                self.send(remote, addr_to_send)
+                logging.info('connecting %s:%d' % (addr, port[0]))
+            except socket.error, e:
+                logging.warn(e)
+                return
+            self.handle_tcp(sock, remote)
+        except socket.error, e:
+            logging.warn(e)
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(__file__) or '.')
+    print 'shadowsocks v1.2'
+
+    with open('config.json', 'rb') as f:
+        config = json.load(f)
+    SERVER = config['server']
+    REMOTE_PORT = config['server_port']
+    PORT = config['local_port']
+    KEY = config['password']
+    METHOD = config.get('method', None)
+
+    argv = sys.argv[1:]
+    if '-6' in sys.argv[1:]:
+        argv.remove('-6')
+
+    optlist, args = getopt.getopt(argv, 's:p:k:l:m:')
+    for key, value in optlist:
+        if key == '-p':
+            REMOTE_PORT = int(value)
+        elif key == '-k':
+            KEY = value
+        elif key == '-l':
+            PORT = int(value)
+        elif key == '-s':
+            SERVER = value
+        elif key == '-m':
+            METHOD = value
+
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
+
+    try:
+        server = ThreadingTCPServer(('', PORT), Socks5Server)
+        logging.info("starting server at port %d ..." % PORT)
+        server.serve_forever()
+    except socket.error, e:
+        logging.error(e)
+    except KeyboardInterrupt:
+        server.shutdown()
+        sys.exit(0)

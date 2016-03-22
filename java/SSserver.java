57c06350938e919e2fc62b07fab351c8afd05985
Besto Apache <bestoapache@gmail.com>
2016-03-22 13:46:13 +0800

    Refine code.

--- java/SSserver.java
+++ java/SSserver.java
@@ -9,119 +9,157 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-public class SSserver
-{
+public class SSserver {
 
-    public static class handleTCP implements Runnable {
-        public Socket src, dst;
-        public handleTCP(Socket l, Socket r){
-            src = l;
-            dst = r;
+    private class SServerInstance implements Runnable {
+
+        final private int HEAD_BUFF_LEN = 64;
+
+        final private int ADDR_TYPE_HOST = 0x03;
+
+        final private int TIMEOUT = 3000;
+
+        private Socket mLocal;
+        private String mRemoteAddr;
+        private int mRemotePort;
+
+        public SServerInstance(Socket l)
+        {
+            mLocal = l;
         }
+
+        private void parseHead(DataInputStream in) throws Exception
+        {
+            int addrtype = in.read();
+            int size = 0;
+            byte buf[] = new byte[HEAD_BUFF_LEN];
+
+            //get addr
+            if (addrtype == ADDR_TYPE_HOST) {
+                size = in.read();
+                size = in.read(buf, 0, size);
+            } else {
+                //do not support other addrtype now.
+                throw new Exception("Unsupport addr type!");
+            }
+            mRemoteAddr = new String(buf, 0, size);
+
+            //get port
+            in.read(buf, 0, 2);
+            ByteBuffer bb = ByteBuffer.allocate(2);
+            bb.order(ByteOrder.BIG_ENDIAN);
+            bb.put(buf[0]);
+            bb.put(buf[1]);
+            mRemotePort = bb.getShort(0);
+
+            System.out.println("Addr type: " + addrtype + " addr: " + mRemoteAddr + ":" + mRemotePort);
+        }
+
         public void run()
         {
-            byte buf[] = new byte[4096];
+            int addrtype;
             int size;
-            try{
-                DataInputStream input = new DataInputStream(src.getInputStream());
-                DataOutputStream output = new DataOutputStream(dst.getOutputStream());
-                while (true) {
-                    size = input.read(buf, 0, 4096);
-                    if (size <= 0) break;
-                    output.write(buf, 0, size);
+
+            try(DataInputStream lin = new DataInputStream(mLocal.getInputStream());
+                    DataOutputStream lout = new DataOutputStream(mLocal.getOutputStream()))
+            {
+
+                parseHead(lin);
+
+                try(Socket remote = new Socket())
+                {
+                    remote.connect(new InetSocketAddress(mRemoteAddr, mRemotePort), TIMEOUT);
+
+                    try(DataInputStream rin = new DataInputStream(remote.getInputStream());
+                            DataOutputStream rout = new DataOutputStream(remote.getOutputStream());)
+                    {
+                        Thread t1 = new Thread(new handleStream(lin, rout));
+                        Thread t2 = new Thread(new handleStream(rin, lout));
+                        t1.start();
+                        t2.start();
+                        t1.join();
+                        t2.join();
+                    }catch(Exception e){
+                        throw e;
+                    }
+                }catch(Exception e){
+                    throw e;
                 }
-            }catch(SocketTimeoutException e){
-                // ignore
-            }catch(Exception e){
-                e.printStackTrace();
-                System.out.println(src.toString());
-                System.out.println(dst.toString());
+
+            }catch (Exception e) {
+                System.err.println("Exception: " + e.getMessage() + "! Target address: " + mRemoteAddr);
+            }finally{
+                try {
+                    mLocal.close();
+                }catch(Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
         }
+    }
+    // FIXME: this is ugly...
+    private class handleStream implements Runnable {
 
+        final private int BUFF_LEN = 4096;
+
+        private DataInputStream mInput;
+        private DataOutputStream mOutput;
+
+        public handleStream(DataInputStream i, DataOutputStream o){
+            mInput = i;
+            mOutput = o;
+        }
+
+        public void run()
+        {
+            byte buf[] = new byte[BUFF_LEN];
+            int r_size, w_size, total_r_size = 0;
+            while (true) {
+                try{
+                    r_size = mInput.read(buf, 0, BUFF_LEN);
+                    total_r_size += r_size;
+                    if (r_size <= 0) 
+                        break;
+                    mOutput.write(buf, 0, r_size);
+                }catch(SocketTimeoutException e){
+                    // ignore
+                    break;
+                }catch(Exception e){
+                    e.printStackTrace();
+                    break;
+                }
+                w_size = mOutput.size();
+                if (total_r_size != w_size) {
+                    System.err.println("Total read size: " + total_r_size + " != Total write size: " + w_size);
+                }
+            }
+        }
     }
 
-    public static class Server implements Runnable {
-
-        private Socket local;
-        private Socket remote;
-        private String addr;
-        private int port;
-
-        public Server(Socket l)
-        {
-            local = l;
-        }
-        public void run() {
-            int addrtype;
-            byte buf[] = new byte[4096];
-            int size;
-
-            try {
-                DataInputStream input = new DataInputStream(local.getInputStream());
-                addrtype = input.read();
-                size = 0;
-                if (addrtype == 3) {
-                    size = input.read();
-                    size = input.read(buf, 0, size);
-                } else {
-                    //do not support other addrtype now.
-                    return;
-                }
-                addr = new String(buf, 0, size);
-
-                input.read(buf, 0, 2);
-                ByteBuffer bb = ByteBuffer.allocate(2);
-                bb.order(ByteOrder.BIG_ENDIAN);
-                bb.put(buf[0]);
-                bb.put(buf[1]);
-
-                port = bb.getShort(0);
-
-                System.out.println("Addr type: " + addrtype + " addr: " + addr + ":" + port);
-
-
-                remote = new Socket();
-                remote.connect(new InetSocketAddress(addr, port), 3000);
-
-                Thread t1 = new Thread(new handleTCP(local, remote));
-                Thread t2 = new Thread(new handleTCP(remote, local));
-                t1.start();
-                t2.start();
-                t1.join();
-                t2.join();
-
-                remote.close();
-                local.close();
-            }catch (Exception e) {
-                System.out.println("Exception:" + e.getMessage() + "! Target address: " + addr);
+    public SSserver()
+    {
+    }
+    
+    public void handle()
+    {
+        int port = 2048;
+        int timeout = 30000;
+        Executor service = Executors.newCachedThreadPool();
+        try(ServerSocket server = new ServerSocket(port)) {
+            server.setReuseAddress(true);
+            while (true) {
+                Socket local = server.accept();
+                local.setSoTimeout(timeout);
+                service.execute(new SServerInstance(local));
             }
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
     public static void main(String argv[])
     {
         System.out.println("Shadowsocks-Java v0.02");
-        int port = 2048;
-        int timeout = 30000;
-        Executor service = Executors.newCachedThreadPool();
-        ServerSocket server = null;
-
-        try {
-            server = new ServerSocket(port);
-            server.setReuseAddress(true);
-        }catch(Exception e){
-            e.printStackTrace();
-            return;
-        }
-        while (true) {
-            try {
-                Socket local = server.accept();
-                local.setSoTimeout(timeout);
-                service.execute(new Server(local));
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
+        new SSserver().handle();;
     }
 }

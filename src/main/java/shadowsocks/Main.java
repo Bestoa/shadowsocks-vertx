88d0e0d6d478e25e5debc0a4a784913b60f1707f
Besto Apache <bestoapache@gmail.com>
2016-07-19 10:00:30 +0800

    Bump

--- src/main/java/shadowsocks/Main.java
+++ src/main/java/shadowsocks/Main.java
@@ -26,7 +26,7 @@ public class Main{
 
     public static Logger log = LogManager.getLogger(Main.class.getName());
 
-    public static final String VERSION = "0.7.1";
+    public static final String VERSION = "0.8";
 
     public static void main(String argv[])
     {

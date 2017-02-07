b1893e0cc31194f2cc6cd7455267fc9a61cfa703
Besto Apache <bestoapache@gmail.com>
2017-02-07 11:17:17 +0800

    Update author.

--- src/main/java/shadowsocks/vertxio/ClientHandler.java
+++ src/main/java/shadowsocks/vertxio/ClientHandler.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2016 Author:NU11 bestoapache@gmail.com
+ *   Copyright 2016 Author:Bestoa bestoapache@gmail.com
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.

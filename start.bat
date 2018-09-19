0fbbda4fd3a3089e1dd23cdcc7537c5270ab0169
lihd <lihd@mail.jj.cn>
2018-09-19 18:05:09 +0800

    。。。

--- start.bat
+++ start.bat
@@ -1,10 +1,8 @@
-# 杀进程
+rem 杀进程
 taskkill /f /im javaw.exe
 
-echo "kill ss success !"
-
-# 暂停 1 秒
+rem 暂停 1 秒
 choice /t 1 /d y /n >nul
 
-# 启动后自动隐藏 cmd 窗口
+rem 启动后自动隐藏 cmd 窗口
 start javaw -jar D:\shadowsocks-fat-0.8.3.jar D:\c.json

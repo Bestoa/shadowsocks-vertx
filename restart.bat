600891f5d9b00d8b03d982b8cf817d0e37f26812
lenovobenben <lenovobenben@gmail.com>
2018-09-20 22:35:55 +0800

    版本升级到 1.0.0

--- restart.bat
+++ restart.bat
@@ -5,6 +5,6 @@ rem 暂停 1 秒
 choice /t 1 /d y /n >nul
 
 rem 启动后自动隐藏 cmd 窗口
-start javaw -jar D:\shadowsocks-fat-0.8.3.jar D:\c.json
+start javaw -jar D:\shadowsocks-fat-1.0.0.jar D:\c.json
 
 rem 如果不支持 ipv6 ，则必须添加jvm参数： java.net.preferIPv4Stack=true

24a80818a3b831b17e2e3660a5df6cc61d3065a5
lihd <lihd@mail.jj.cn>
2018-09-20 15:49:48 +0800

    精简 TEST.java 代码，只保留 SOCKS 客户端功能

--- src/main/java/shadowsocks/TEST.java
+++ src/main/java/shadowsocks/TEST.java
@@ -30,24 +30,11 @@ public class TEST {
 
     private static void testSimpleHttp() {
 
-        ShadowsocksVertx server = new ShadowsocksVertx(true);
-        ShadowsocksVertx client = new ShadowsocksVertx(false);
-
-        server.start();
-        client.start();
-
-        //Wait 2s for server/client start.
-        try{
-            Thread.sleep(2000);
-        }catch(Exception e){
-            //ignore
-        }
-
         Proxy proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", GlobalConfig.get().getLocalPort()));
         HttpURLConnection proxyConnection = null;// 代理连接
         HttpURLConnection directConnection = null;// 直接连接
         try{
-            URL url = new URL("https://www.baidu.com");
+            URL url = new URL("http://www.example.com/");
 
             proxyConnection = (HttpURLConnection)url.openConnection(proxy);
             proxyConnection.setRequestMethod("GET");
@@ -56,17 +43,17 @@ public class TEST {
             directConnection.setRequestMethod("GET");
 
             DataInputStream proxyData = new DataInputStream(proxyConnection.getInputStream());
-            byte [] result = new byte[10000];
-            proxyData.read(result);// 代理数据放入 result
-            log.info("proxy \n\n " + new String(result,"UTF-8"));
+            byte [] proxyArr = new byte[1024];
+            proxyData.read(proxyArr);// 代理数据放入 proxyArr
+            log.info("proxy \n\n " + new String(proxyArr,"UTF-8"));
 
             System.out.println("------------------------------------------------------------------------------------------");
             System.out.println("------------------------------------------------------------------------------------------");
 
             DataInputStream directData = new DataInputStream(directConnection.getInputStream());
-            byte [] expect = new byte[10000];
-            directData.read(expect);// 直连数据放入 expect
-            log.info("direct \n\n " + new String(expect,"UTF-8"));
+            byte [] directArr = new byte[1024];
+            directData.read(directArr);// 直连数据放入 directArr
+            log.info("direct \n\n " + new String(directArr,"UTF-8"));
 
         }catch(IOException e){
             log.error("Failed with exception.", e);
@@ -77,14 +64,7 @@ public class TEST {
             if (directConnection != null) {
                 directConnection.disconnect();
             }
-            server.stop();
-            client.stop();
-            //Wait 1s for server/client stop.
-            try{
-                Thread.sleep(1000);
-            }catch(Exception e){
-                //ignore
-            }
+
         }
     }
 }
